d8142db9ffc328f3585f56ab2d9d99c68f821c48
Alberto Baesso <64778418+ABaesso@users.noreply.github.com>
2022-05-13 21:41:43 +0200

    prova modifica grafica

--- website/models.py
+++ website/models.py
@@ -3,17 +3,52 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
+class Studenti(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(150))
+    cognome = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(256))
+    data_nascita = db.Column(db.Date())
+    scuola = db.Column(db.String(150))  # possibilmente diventa chiave esterna
+    data_creazione = db.Column(db.DateTime(timezone=True), default=func.now())  # serve?
+
+
+class Professori(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(150))
+    cognome = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(256))
+    data_nascita = db.Column(db.Date())
+    is_admin = db.Column(db.Boolean, default=False)
+    data_creazione = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    data_creazione = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)  # chiave esterna da users
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'post.id', ondelete="CASCADE"), nullable=False)
